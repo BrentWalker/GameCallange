0a6f343b8d978fd4b301cebf903fbb2c1dc419db
CerebroX <cerebro@CerebroXs-MacBook-Pro.local>
2019-04-13 11:03:10 -0400

    made js comments

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+score starts at 0
+click on pad of .allPads
+pad disables after use
+question box w 3 multiple choice answers
+16 questions
+correct answer adds to score
+incorrect answer subtracts from score
+win condition is earn 3500
+lose condition is earn -2000
+upon winning allPads change to drumPads
+*/ 
