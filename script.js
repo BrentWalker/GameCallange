315bbacbf8fbeb682c232168de349112cafbd211
CerebroX <cerebro@CerebroXs-MacBook-Pro.local>
2019-04-14 11:29:49 -0400

    added variablesin js fixed div buttons

--- script.js
+++ script.js
@@ -21,8 +21,37 @@ upon winning allPads change to drumPads
 // -----------------------variables-------------------
 let score = 0
 
-let screen = document.getElementById("screen")
+let qScreen = document.getElementById("qScreen")
 
+let sScreen = document.getElementById("sScreen")
+
+let pad16 = document.getElementById("pad16")
+let pad15 = document.getElementById("pad15")
+let pad14 = document.getElementById("pad14")
+let pad13 = document.getElementById("pad13")
+let pad12 = document.getElementById("pad12")
+let pad11 = document.getElementById("pad11")
+let pad10 = document.getElementById("pad10")
+let pad09 = document.getElementById("pad09")
+let pad08 = document.getElementById("pad08")
+let pad07 = document.getElementById("pad07")
+let pad06 = document.getElementById("pad06")
+let pad05 = document.getElementById("pad05")
+let pad04 = document.getElementById("pad04")
+let pad03 = document.getElementById("pad03")
+let pad02 = document.getElementById("pad02")
+let pad01 = document.getElementById("pad01")
+
+
+
+
+// -------------------------functions------------------
+
+// add questions and answers
+// choose answer
+// check if answer is correct
+// sum score
+// win/lose/drum
 
 
 
